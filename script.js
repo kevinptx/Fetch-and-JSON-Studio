e8bcb31166cb2ba13f51099dbf703301a798df42
Kevin Peery <kevin.peery@gmail.com>
2022-08-16 00:19:51 -0500

    add bonus

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-window.addEventListener("load", function(){
-    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
-   .then((response) => response.json()) 
-    .then((jsonData) => {
-        const divContainer = document.getElementById("container")
-        let astronauts = "";
-        for (astronaut of jsonData) {
-            astronauts += `
-            <div class="astronaut">
-       <div class="bio">
-          <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
-          <ul>
-             <li>Hours in space: ${astronaut.hoursInSpace}</li>
-             <li>Active: ${astronaut.active}</li>
-             <li>Skills: ${astronaut.skills.join(", ")}</li>
-          </ul>
-       </div>
-       <img class="avatar" src="${astronaut.picture}">
-    </div> `
-
-        }
-    divContainer.innerHTML = astronauts;
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            let container = document.getElementById("container");
+            json.sort((a, b) => b.hoursInSpace - a.hoursInSpace)
+            for (astronaut of json) {
+                container.innerHTML +=
+                    `<div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li id= "${astronaut.id}">Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src=${astronaut.picture}>
+            </div>`
+                if (astronaut.active === true) {
+                    document.getElementById(astronaut.id).style.color = "green"
+                }
+            }
+            document.getElementById("count").innerHTML += `Total number of astronauts: ${Object.keys(json).length}`
+        })
     })
-});
+})
